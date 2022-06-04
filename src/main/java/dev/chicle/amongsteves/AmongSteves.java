8456d68011f8d2996136ee6c458f4c9e31fc8adc
Chicle <chicle@protonmail.com>
2022-06-04 17:21:23 +0200

    First commit

---
+++ src/main/java/dev/chicle/amongsteves/AmongSteves.java
@@ -0,0 +1,17 @@
+package dev.chicle.amongsteves;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class AmongSteves extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
