2f9b77b5f4bfe029b2732d869a26f3d4b647ae8d
Chicle <hola@chicle.dev>
2022-06-06 22:07:39 +0200

    Usar logger para el cambio de estado

--- src/main/java/dev/chicle/amongsteves/gamestate/listener/GameStateChangeListener.java
+++ src/main/java/dev/chicle/amongsteves/gamestate/listener/GameStateChangeListener.java
@@ -4,10 +4,11 @@ import dev.chicle.amongsteves.gamestate.event.GameStateChangeEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import static org.bukkit.Bukkit.getLogger;
+
 public class GameStateChangeListener implements Listener {
     @EventHandler
     public void onGameStateChange(GameStateChangeEvent ev){
-        System.out.println(ev.getOldGameState().toString());
-        System.out.println(ev.getNewGameState().toString());
+        getLogger().info("GameStateChangeEvent: " + ev.getOldGameState().name() + " -> " + ev.getNewGameState().name());
     }
 }
