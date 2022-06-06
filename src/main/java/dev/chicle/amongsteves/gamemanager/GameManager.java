7fa87edd93567050663fe75cbfafeb8b809aa43f
Chicle <chicle@protonmail.com>
2022-06-07 01:07:33 +0200

    Teletransportar al jugador al cambiar estado

--- src/main/java/dev/chicle/amongsteves/gamemanager/GameManager.java
+++ src/main/java/dev/chicle/amongsteves/gamemanager/GameManager.java
@@ -5,6 +5,8 @@ import dev.chicle.amongsteves.gamestate.event.GameStateChangeEvent;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 
+import static org.bukkit.Bukkit.getLogger;
+
 public class GameManager {
     @Getter
     private static GameState state;
@@ -12,12 +14,12 @@ public class GameManager {
     public GameManager() {
         state = GameState.IN_LOBBY;
     }
-
     /**
      * @param state El nuevo estado del juego
      */
     public void setState(GameState state) {
         Bukkit.getPluginManager().callEvent(new GameStateChangeEvent(getState(), state));
+        getLogger().info("GameStateChangeEvent: " + getState() + " -> " + state);
         GameManager.state = state;
     }
 }
--- src/main/java/dev/chicle/amongsteves/gamemanager/listener/PlayerJoinListener.java
+++ src/main/java/dev/chicle/amongsteves/gamemanager/listener/PlayerJoinListener.java
@@ -1,44 +1,26 @@
 package dev.chicle.amongsteves.gamemanager.listener;
 
 import dev.chicle.amongsteves.AmongSteves;
+import dev.chicle.amongsteves.config.Locations;
 import dev.chicle.amongsteves.gamemanager.GameManager;
 import dev.chicle.amongsteves.gamestate.GameState;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
-    private final AmongSteves plugin;
-
-    public PlayerJoinListener(AmongSteves plugin) {
-        this.plugin = plugin;
-    }
-
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent ev) {
-        FileConfiguration config = plugin.getConfig();
-        Player player = ev.getPlayer();
+        Player p = ev.getPlayer();
 
-        switch (GameManager.getState()){
+        if (GameManager.getState() == GameState.IN_LOBBY) {
+            p.teleport(Locations.getLobby());
         }
 
-        Location lobby = new Location(
-                player.getWorld(),
-                config.getDouble("lobby.coords.x"),
-                config.getDouble("lobby.coords.y"),
-                config.getDouble("lobby.coords.z"),
-                (float) config.getDouble("lobby.coords.yaw"),
-                (float) config.getDouble("lobby.coords.pitch"));
-
-        player.teleport(lobby);
-
-        player.sendMessage(
-                ChatColor.AQUA + "[AmongSteves] " +
-                        ChatColor.RESET + "Bienvenido! Actualmente la partida esta " +
-                        ChatColor.ITALIC + GameManager.getState().getLabel() + ChatColor.RESET + ".");
+        p.sendMessage(AmongSteves.chatPrefix +
+                ChatColor.RESET + "Bienvenido! Actualmente la partida esta " +
+                ChatColor.ITALIC + GameManager.getState().getLabel() + ChatColor.RESET + ".");
     }
 }
