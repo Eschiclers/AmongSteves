e18785798caf78519846fb8e1802d65e84701898
Chicle <chicle@protonmail.com>
2022-06-07 23:25:13 +0200

    Elige impostores entre los players al empezar

--- src/main/java/dev/chicle/amongsteves/gamemanager/GameManager.java
+++ src/main/java/dev/chicle/amongsteves/gamemanager/GameManager.java
@@ -6,12 +6,20 @@ import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.bukkit.Bukkit.getLogger;
 
 public class GameManager {
     @Getter
     private static GameState state;
-    private static ASPlayer[] players;
+    private static List<ASPlayer> players = new ArrayList<>();
+
+    // Ajustes del juego
+    @Getter private static int maxPlayers = 6;
+    @Getter private static int minPlayers = 10;
+    @Getter private static int maxImpostors = 1;
 
     public GameManager() {
         state = GameState.IN_LOBBY;
@@ -20,49 +28,34 @@ public class GameManager {
     /**
      * @param state El nuevo estado del juego
      */
-    public void setState(GameState state) {
+    public static void setState(GameState state) {
         Bukkit.getPluginManager().callEvent(new GameStateChangeEvent(getState(), state));
         getLogger().info("GameStateChangeEvent: " + getState() + " -> " + state);
         GameManager.state = state;
     }
 
     public static void addPlayer(ASPlayer player) {
-        if (players == null) {
-            players = new ASPlayer[1];
-        } else {
-            ASPlayer[] newPlayers = new ASPlayer[players.length + 1];
-            System.arraycopy(players, 0, newPlayers, 0, players.length);
-            players = newPlayers;
-        }
-        players[players.length - 1] = player;
+        players.add(player);
     }
 
     public static void removePlayer(ASPlayer player) {
-        if (players == null) {
-            return;
-        }
-        ASPlayer[] newPlayers = new ASPlayer[players.length - 1];
-        int i = 0;
-        for (ASPlayer p : players) {
-            if (p != player) {
-                newPlayers[i] = p;
-                i++;
-            }
-        }
-        players = newPlayers;
+        players.remove(player);
     }
 
-    public static ASPlayer[] getPlayers() {
+    public static List<ASPlayer> getPlayers() {
         return players;
     }
 
     public static ASPlayer getPlayer(Player player) {
-        if (players == null) return null;
         for (ASPlayer p : players) {
-            if (p.getPlayer() == player) {
+            if (p.getPlayer().equals(player)) {
                 return p;
             }
         }
         return null;
+    }
+
+    public static void startGame() {
+        setState(GameState.IN_GAME);
     }
 }
--- src/main/java/dev/chicle/amongsteves/gamemanager/listener/GameStateChangeListener.java
+++ src/main/java/dev/chicle/amongsteves/gamemanager/listener/GameStateChangeListener.java
@@ -4,27 +4,32 @@ import dev.chicle.amongsteves.AmongSteves;
 import dev.chicle.amongsteves.config.Locations;
 import dev.chicle.amongsteves.gamemanager.GameManager;
 import dev.chicle.amongsteves.event.GameStateChangeEvent;
+import dev.chicle.amongsteves.gamemanager.player.ASPlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import java.util.List;
-
 public class GameStateChangeListener implements Listener {
     @EventHandler
-    public void onGameStateChange(GameStateChangeEvent ev){
+    public void onGameStateChange(GameStateChangeEvent ev) {
 
         switch (ev.getNewGameState()) {
-            case IN_GAME -> List.of(GameManager.getPlayers()).forEach(p -> {
-                p.getPlayer().teleport(Locations.getStart());
-                p.getPlayer().sendMessage(AmongSteves.chatPrefix +
-                        ChatColor.RESET + "La partida ha comenzado!");
-            });
-            case IN_LOBBY -> List.of(GameManager.getPlayers()).forEach(p -> {
-                p.getPlayer().teleport(Locations.getLobby());
-                p.getPlayer().sendMessage(AmongSteves.chatPrefix +
-                        ChatColor.RESET + "La partida ha terminado. Esperando a que comience una nueva partida.");
-            });
+            case IN_GAME:
+                for (ASPlayer p : GameManager.getPlayers()) {
+                    p.getPlayer().teleport(Locations.getStart());
+                    p.getPlayer().sendMessage(AmongSteves.chatPrefix + ChatColor.GREEN + "La partida ha empezado!");
+                }
+                break;
+            case IN_LOBBY: {
+                for (ASPlayer p : GameManager.getPlayers()) {
+                    p.getPlayer().teleport(Locations.getLobby());
+                    p.getPlayer().sendMessage(AmongSteves.chatPrefix +
+                            ChatColor.RESET + "La partida ha finalizado!");
+                }
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown game state: " + ev.getNewGameState());
         }
     }
 }
