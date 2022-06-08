4756945897d2f9c123c656b0fce697f04aeadd80
Chicle <chicle@protonmail.com>
2022-06-08 03:29:42 +0200

    Preparando el enum de PlayerColor para las skins

--- src/main/java/dev/chicle/amongsteves/gamemanager/player/PlayerColor.java
+++ src/main/java/dev/chicle/amongsteves/gamemanager/player/PlayerColor.java
@@ -1,5 +1,33 @@
 package dev.chicle.amongsteves.gamemanager.player;
 
 public enum PlayerColor {
-    BLACK, BLUE, BROWN, CYAN, GREEN, LIME, ORANGE, PINK, PURPLE, RED, WHITE, YELLOW;
+    BLACK("", ""),
+    BLUE("", ""),
+    BROWN("", ""),
+    CYAN("", ""),
+    GREEN("", ""),
+    LIME("", ""),
+    ORANGE("", ""),
+    PINK("", ""),
+    PURPLE("", ""),
+    RED("", ""),
+    WHITE("", ""),
+    YELLOW("", "");
+
+
+    private final String texture;
+    private final String signature;
+
+    PlayerColor(String texture, String signature) {
+        this.texture = texture;
+        this.signature = signature;
+    }
+
+    public String getTexture() {
+        return texture;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
 }
