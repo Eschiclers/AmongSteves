0e719840e36dfbc461eab48d3e84c6fc86dce9dd
Chicle <chicle@protonmail.com>
2022-06-08 04:10:56 +0200

    Se inicia la partida por el startGame del GameManager

--- src/main/java/dev/chicle/amongsteves/command/StartGameCommand.java
+++ src/main/java/dev/chicle/amongsteves/command/StartGameCommand.java
@@ -1,41 +1,16 @@
 package dev.chicle.amongsteves.command;
 
 import dev.chicle.amongsteves.gamemanager.GameManager;
-import dev.chicle.amongsteves.gamemanager.GameState;
-import dev.chicle.amongsteves.gamemanager.player.ASPlayer;
-import dev.chicle.amongsteves.gamemanager.player.PlayerRole;
-import net.md_5.bungee.api.ChatMessageType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class StartGameCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) return true;
-        Player p = (Player) sender;
-
-        GameManager.setState(GameState.IN_GAME);
-
-        List<ASPlayer> impostors = new ArrayList<>();
-
-        Random random = new Random();
-
-        while (impostors.size() < GameManager.getMaxImpostors()) {
-            ASPlayer impostor = GameManager.getPlayers().get(random.nextInt(GameManager.getPlayers().size()));
-            if (impostors.contains(impostor)) continue;
-            impostors.add(impostor);
-            impostor.setRole(PlayerRole.IMPOSTOR);
-        }
-
-        sender.sendMessage("Eres " + GameManager.getPlayer((Player) sender).getRole().toString() + ". " + impostors.size() + "/" + GameManager.getMaxImpostors() + ".");
-
-
+        GameManager.startGame();
         return true;
     }
 }
