8b9ac3ae2aeb1d43b91833a812e27517872d6e96
Chicle <chicle@protonmail.com>
2022-06-11 23:22:00 +0200

    El plugin tirará throw y se desactivará si hay más jugadores que colores

--- src/main/java/dev/chicle/amongsteves/AmongSteves.java
+++ src/main/java/dev/chicle/amongsteves/AmongSteves.java
@@ -3,7 +3,9 @@ package dev.chicle.amongsteves;
 import dev.chicle.amongsteves.command.SetupCommand;
 import dev.chicle.amongsteves.config.SetupConfiguration;
 import dev.chicle.amongsteves.gamemanager.GameManager;
+import dev.chicle.amongsteves.gamemanager.player.PlayerColor;
 import dev.chicle.amongsteves.listener.SetupListener;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,6 +21,12 @@ public final class AmongSteves extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        if(Bukkit.getMaxPlayers() > PlayerColor.values().length){
+            Bukkit.getPluginManager().disablePlugin(this);
+            throw new RuntimeException("La cantidad de jugadores no puede ser mayor a la cantidad de colores disponibles.");
+        }
+
         instance = this;
         // Plugin startup logic
         new SetupConfiguration(this);
