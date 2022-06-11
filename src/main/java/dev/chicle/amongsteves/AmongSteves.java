39fb03dbdb0d5aeae78da2c6a960594ce3b328bf
Chicle <chicle@protonmail.com>
2022-06-12 01:12:25 +0200

    Logger propio (solo por el prefijo)

--- src/main/java/dev/chicle/amongsteves/AmongSteves.java
+++ src/main/java/dev/chicle/amongsteves/AmongSteves.java
@@ -3,8 +3,10 @@ package dev.chicle.amongsteves;
 import dev.chicle.amongsteves.command.SetupCommand;
 import dev.chicle.amongsteves.config.SetupConfiguration;
 import dev.chicle.amongsteves.gamemanager.GameManager;
+import dev.chicle.amongsteves.gamemanager.Teams;
 import dev.chicle.amongsteves.gamemanager.player.PlayerColor;
 import dev.chicle.amongsteves.listener.SetupListener;
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -13,9 +15,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 @SuppressWarnings("unused")
 public final class AmongSteves extends JavaPlugin {
     private static AmongSteves instance;
+    @Getter
+    private static Log log;
+    public static final String prefix = "[AmongSteves] ";
     FileConfiguration config = getConfig();
 
-    public static final String chatPrefix = ChatColor.AQUA + "[AmongSteves] " + ChatColor.RESET;
+    public static final String chatPrefix = ChatColor.AQUA + prefix + ChatColor.RESET;
 
     public static GameManager GameManager;
 
@@ -28,12 +33,14 @@ public final class AmongSteves extends JavaPlugin {
         }
 
         instance = this;
+        log = new Log(this);
         // Plugin startup logic
         new SetupConfiguration(this);
         new SetupListener(this);
         new SetupCommand(this);
 
         GameManager = new GameManager(this);
+        Teams.registerTeams();
     }
 
     @Override
